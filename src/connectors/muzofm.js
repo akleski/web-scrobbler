8a3f3e06d2ad83f390be04cb1d3d9dfcd6dfc7de
Adam Kleski <akleski@gmail.com>
2018-02-08 11:29:35 +0100

    Improved handling of isPlaying (). It detects better the song transitions. Unless 2 or more songs are played in a row.

--- src/connectors/muzofm.js
+++ src/connectors/muzofm.js
@@ -6,4 +6,5 @@ Connector.artistSelector = '.player__label--band';
 
 Connector.trackSelector = '.player__label--title';
 
-Connector.isPlaying = () => $('#player').hasClass('playing');
+// Connector.isPlaying = () => $('#player').hasClass('playing');
+Connector.isPlaying = () => !$('.player__labels').hasClass('player__labels--off');
